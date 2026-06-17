944b79f32214d39c78a57a007b49bc564972014e
Sultan <sultan.tuleulov2003@gmail.com>
2026-06-17 20:18:28 +0500

    updated operations tests

--- tests/test_api/test_operations.py
+++ tests/test_api/test_operations.py
@@ -1,11 +1,14 @@
+from datetime import timedelta
 from decimal import Decimal
 from app.enum import CurrencyEnum
 from app.models import User, Wallet
+from auth import create_access_token, hash_password
+from config import settings
 
 
 def test_add_expense_success(db_session, client):
     # Arrange
-    user = User(login="test")
+    user = User(login="test", password_hash=hash_password("123"))
     db_session.add(user)
     db_session.flush()
     wallet = Wallet(
@@ -14,12 +17,17 @@ def test_add_expense_success(db_session, client):
     db_session.add(wallet)
     db_session.commit()
     db_session.refresh(wallet)
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+
+    access_token = create_access_token(
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
+    )
 
     # Act
     response = client.post(
         "/api/v1/operations/expense",
         json={"wallet_name": "card", "amount": 50.0, "description": "bubblegum"},
-        headers={"Authorization": f"Bearer {user.login}"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
 
     # Assert
@@ -27,25 +35,33 @@ def test_add_expense_success(db_session, client):
     assert response.json()["type"] == "expense"
     assert response.json()["wallet_id"] == wallet.id
     assert Decimal(str(response.json()["amount"])) == Decimal(50)
-    # assert Decimal(str(response.json()["new_balance"])) == Decimal(150)
+    assert Decimal(str(response.json()["new_balance"])) == Decimal(150)
     assert response.json()["category"] == "bubblegum"
+    assert response.json()["currency"] == CurrencyEnum.KZT
 
 
 def test_add_expense_negative_amount(db_session, client):
     # Arrange
-    user = User(login="test")
+    user = User(login="test", password_hash=hash_password("123"))
     db_session.add(user)
     db_session.flush()
-    wallet = Wallet(name="card", balance=200, user_id=user.id)
+    wallet = Wallet(
+        name="card", balance=200, user_id=user.id, currency=CurrencyEnum.KZT
+    )
     db_session.add(wallet)
     db_session.commit()
     db_session.refresh(wallet)
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+
+    access_token = create_access_token(
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
+    )
 
     # Act
     response = client.post(
         "/api/v1/operations/expense",
         json={"wallet_name": "card", "amount": -50.0, "description": "bubblegum"},
-        headers={"Authorization": f"Bearer {user.login}"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
 
     # Assert
@@ -54,19 +70,26 @@ def test_add_expense_negative_amount(db_session, client):
 
 def test_add_expense_name_is_empty(db_session, client):
     # Arrange
-    user = User(login="test")
+    user = User(login="test", password_hash=hash_password("123"))
     db_session.add(user)
     db_session.flush()
-    wallet = Wallet(name="card", balance=200, user_id=user.id)
+    wallet = Wallet(
+        name="card", balance=200, user_id=user.id, currency=CurrencyEnum.KZT
+    )
     db_session.add(wallet)
     db_session.commit()
     db_session.refresh(wallet)
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+
+    access_token = create_access_token(
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
+    )
 
     # Act
     response = client.post(
         "/api/v1/operations/expense",
         json={"wallet_name": "  ", "amount": 50.0, "description": "bubblegum"},
-        headers={"Authorization": f"Bearer {user.login}"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
 
     # Assert
@@ -75,15 +98,21 @@ def test_add_expense_name_is_empty(db_session, client):
 
 def test_add_expense_wallet_not_existing(db_session, client):
     # Arrange
-    user = User(login="test")
+    user = User(login="test", password_hash=hash_password("123"))
     db_session.add(user)
+    db_session.flush()
     db_session.commit()
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+
+    access_token = create_access_token(
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
+    )
 
     # Act
     response = client.post(
         "/api/v1/operations/expense",
         json={"wallet_name": "card", "amount": 50.0, "description": "bubblegum"},
-        headers={"Authorization": f"Bearer {user.login}"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
 
     # Assert
@@ -92,13 +121,20 @@ def test_add_expense_wallet_not_existing(db_session, client):
 
 def test_add_expense_unauthorized(db_session, client):
     # Arrange
-    user = User(login="test")
+    user = User(login="test", password_hash=hash_password("123"))
     db_session.add(user)
     db_session.flush()
-    wallet = Wallet(name="card", balance=200, user_id=user.id)
+    wallet = Wallet(
+        name="card", balance=200, user_id=user.id, currency=CurrencyEnum.KZT
+    )
     db_session.add(wallet)
     db_session.commit()
     db_session.refresh(wallet)
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+
+    access_token = create_access_token(
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
+    )
     # Act
     response = client.post(
         "/api/v1/operations/expense",
@@ -112,18 +148,25 @@ def test_add_expense_unauthorized(db_session, client):
 
 def test_add_expense_insufficient_funds(db_session, client):
     # Arrange
-    user = User(login="test")
+    user = User(login="test", password_hash=hash_password("123"))
     db_session.add(user)
     db_session.flush()
-    wallet = Wallet(name="card", balance=200, user_id=user.id)
+    wallet = Wallet(
+        name="card", balance=200, user_id=user.id, currency=CurrencyEnum.KZT
+    )
     db_session.add(wallet)
     db_session.commit()
     db_session.refresh(wallet)
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+
+    access_token = create_access_token(
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
+    )
     # Act
     response = client.post(
         "/api/v1/operations/expense",
         json={"wallet_name": "card", "amount": 300.0, "description": "bubblegum"},
-        headers={"Authorization": f"Bearer {user.login}"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
 
     # Assert
